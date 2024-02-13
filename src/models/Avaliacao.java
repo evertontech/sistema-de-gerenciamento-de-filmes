6f48d75ec0f5013bec488888e5f3252765672fb8
Everton Santos <everton_projeto@outlook.com>
2024-02-13 15:38:21 -0300

    criando classe avaliacao

---
+++ src/models/Avaliacao.java
@@ -0,0 +1,26 @@
+package models;
+
+public class Avaliacao {
+    private Critico critico;
+    private Double pontuacao;
+
+    public void setCritico(Critico critico){
+        this.critico = critico;
+    }
+
+    public Critico getCritico() {
+        return this.critico;
+    }
+
+    public void setPontuacao(Double pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public Double getPontuacao() {
+        return this.pontuacao;
+    }
+
+    public String toString() {
+        return "Crítico: " + this.critico + "Pontuação: " + this.pontuacao;
+    }
+}
