94e56fe5e749774bf254a33f861a1e46b6dcf693
Everton Santos <everton_projeto@outlook.com>
2024-03-04 15:22:06 -0300

    add construtor classe critico

--- src/models/Critico.java
+++ src/models/Critico.java
@@ -5,6 +5,11 @@ import enums.VeiculoDeCritica;
 public class Critico extends Pessoa {
     private VeiculoDeCritica veiculoDeCritica;
 
+    public Critico(String nome, Integer idade, VeiculoDeCritica veiculoDeCritica) {
+        super(nome, idade);
+        this.setVeiculoDeCritica(veiculoDeCritica);
+    }
+
     public void setVeiculoDeCritica(VeiculoDeCritica veiculoDeCritica) {
         this.veiculoDeCritica = veiculoDeCritica;
     }
