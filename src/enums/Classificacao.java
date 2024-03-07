a0fe16607ff82178d3e9f3da1c7570dda0f50381
Everton Santos <everton_projeto@outlook.com>
2024-03-07 15:27:47 -0300

    criando to string no enum classificacao

--- src/enums/Classificacao.java
+++ src/enums/Classificacao.java
@@ -1,10 +1,20 @@
 package enums;
 
 public enum Classificacao {
-    LIVRE,
-    MAIOR_10,
-    MAIOR_12,
-    MAIOR_14,
-    MAIOR_16,
-    MAIOR_18
+    LIVRE("Livre"),
+    MAIOR_10("Maior que 10 anos de idade."),
+    MAIOR_12("Maior que 12 anos de idade."),
+    MAIOR_14("Maior que 14 anos de idade."),
+    MAIOR_16("Maior que 16 anos de idade."),
+    MAIOR_18("Maior que 18 anos de idade.");
+
+    private final String nome;
+
+    Classificacao(String nome) {
+        this.nome = nome;
+    }
+
+    public String toString() {
+        return this.nome;
+    }
 }
